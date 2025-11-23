34292f4063e0abb0818c8cb2322142e440cc5e94
Vrishank <Kesavan_Vrishank_Raghav@ichat.sp.edu.sg>
2025-11-23 23:23:28 +0800

    done

--- bmi.py
+++ bmi.py
@@ -1,12 +1,23 @@
 def calculate_bmi(height, weight):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
+    print("Height =", height)
+    print("Weight =", weight)
 
-    # Add code here to calculate BMI
+    # Calculate BMI
     bmi = weight / (height * height)
 
-    # Add code here to display calculated BMI
+    # Display calculated BMI
     print("BMI =", bmi)
 
+    # Determine weight classification
+    if bmi < 18.5:
+        print("Weight Classification: Under Weight")
+    elif bmi <= 25.0:
+        print("Weight Classification: Normal Weight")
+    else:
+        print("Weight Classification: Over Weight")
+
+
+# Test the function
 calculate_bmi(weight=57, height=1.73)
+
 
