d6dd3a99660f64305e40943be55621dbdfd807f4
Vrishank <Kesavan_Vrishank_Raghav@ichat.sp.edu.sg>
2025-11-24 00:09:55 +0800

    Ex3

--- bmi.py
+++ bmi.py
@@ -1,23 +1,26 @@
 def calculate_bmi(height, weight):
-    print("Height =", height)
-    print("Weight =", weight)
+  print("Height = " + str(height))
+  print("Weight = " + str(weight))
 
-    # Calculate BMI
-    bmi = weight / (height * height)
+  bmi = weight/(height*height)
 
-    # Display calculated BMI
-    print("BMI =", bmi)
+  print("BMI = " + str(bmi))
 
-    # Determine weight classification
-    if bmi < 18.5:
-        print("Weight Classification: Under Weight")
-    elif bmi <= 25.0:
-        print("Weight Classification: Normal Weight")
-    else:
-        print("Weight Classification: Over Weight")
+  if bmi < 18.5:
+    print("Under Weight")
+    return -1
 
+  elif bmi > 25.0:
+    print("Over Weight")
+    return 1
 
-# Test the function
-calculate_bmi(weight=57, height=1.73)
+  else:
+    print("Normal Weight")
+    return 0
 
+def main():
+  print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+  calculate_bmi(weight=57, height=1.73)
 
+if __name__ == "__main__":
+    main()
--- lab2.py
+++ lab2.py
@@ -1,1 +1,43 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
+
+
+def get_user_input():
+    user_input = input()
+    string_list = user_input.split(",")
+
+    float_list = []
+    for item in string_list:
+        float_list.append(float(item))
+
+    return float_list
+
+
+def calc_average_temperature(temp_list):
+    total = sum(temp_list)
+    count = len(temp_list)
+    return total / count
+
+
+def calc_min_max_temperature(temp_list):
+    minimum = min(temp_list)
+    maximum = max(temp_list)
+    return [minimum, maximum]
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+
+    a = get_user_input()
+    b = calc_average_temperature(a)
+    c = calc_min_max_temperature(a)
+
+    print("Average:", b)
+    print("Minimum:", c[0])
+    print("Maximum:", c[1])
+
+
+# Correct syntax:
+if __name__ == "__main__":
+    main()
